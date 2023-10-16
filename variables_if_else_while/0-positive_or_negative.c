9db79b9e4450222eda52e3b86248302aad354b2f
Chocolat-David <7573@holbertonstudents.com>
2023-10-16 07:48:02 -0700

    Premier programme en C : creer un entier aleatoire

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
@@ -10,15 +11,13 @@ int main(void)
     srand(time(0));
     n = rand() - RAND_MAX 2;
     /* your code goes there */
-    if ( n > 0 ) {
-      printf("is positive\n");
+    if (n > 0) {
+      printf("%d is positive\n", n);
     } else if ( n = 0 ) {
-      printf("is zero\n");
+      printf("%d is zero\n", n);
     } else {
-      printf("is negative\n");
+      printf("%d is negative\n", n);
     }
-
-    printf("la valeur de n est : %d\n", n);
 
     return (0);
 }
