4377a575847bddef407a378064cbba29714e61b4
Chocolat-David <7573@holbertonstudents.com>
2023-10-18 12:30:33 -0700

    Premier programme en C : teste le dernier digit de la variable n

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+* main - main function
+*
+* Return: char
+*/
+
+int main(void)
+{
+	int i = '0';
+	char c = 'a';
+
+	while (i <= '9')
+		{
+		putchar(i);
+		i++;
+		}
+
+	while (c <= 'f')
+		{
+		putchar(c);
+		c++;
+		}
+	putchar('\n');
+
+	return (0);
+}
