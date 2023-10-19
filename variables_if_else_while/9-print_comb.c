f53ef45c7678571bfdda7922ee4f449d055414c0
Chocolat-David <7573@holbertonstudents.com>
2023-10-19 01:33:57 -0700

    premier programmes en c et gertion de l'affichage

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,13 +10,15 @@ int main(void)
 {
 	int n = '0';
 
-	while (n <= '9')
+	while (n <= '8')
 		{
 		putchar(n);
 		putchar(',');
 		putchar(' ');
 		n++;
 		}
+	n++;
+	putchar(n);
 	putchar('\n');
 
 	return (0);
