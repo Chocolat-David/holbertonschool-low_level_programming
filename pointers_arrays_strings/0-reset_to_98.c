3bf030b93224021ef323b933201a83f6f961a3be
Chocolat-David <7573@holbertonstudents.com>
2023-10-26 05:00:36 -0700

    fonction

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include "main.h"
 
-/* fonction reset */
+/* fonction reset
+ * Write a function that takes a pointer to an int as parameter and updates the value it points to to 98
+ * */
 
 void reset_to_98(int *n)
 {
